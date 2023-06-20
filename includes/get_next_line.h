cfaff3314bb6972b6cf39e129f0230ea267fd4cb
Luis <luissoto2094@gmail.com>
2023-06-20 16:26:51 +0200

    should pass norm now

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsoto-do <lsoto-do@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 16:24:49 by lsoto-do          #+#    #+#             */
+/*   Updated: 2023/06/20 16:25:02 by lsoto-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -8,7 +19,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-extern  int BUFFER_SIZE;
 void	*ft_free_strs_line(char **str, char **str2);
 char	*get_next_line(int fd, int b_size);
 char	*read_file(int fd, char *buffer, int bytes_read, int b_size);
